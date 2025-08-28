4912e29a7988dec00261990e272b6b8a5967accd
vermarajkumar81770-crypto <vermarajkumar81770@gmail.com>
2025-08-28 09:04:33 +0530

    Add files via upload

---
+++ basic mathematical.py
@@ -0,0 +1,15 @@
+# take two numbers as input from the user
+num1 =  float (input("enter the first number:"))
+num2 =  float (input("enter the second number:"))
+# perform basic mathematical oprations
+
+addition = num1 + num2
+subtaction = num1-num2
+multiplication = num1 * num2
+division = num1/num2
+#display the result
+print("addition:",addition)
+print("subtaction:",subtaction)
+print("multiplication:",multiplication)
+print("division:",division)
+
